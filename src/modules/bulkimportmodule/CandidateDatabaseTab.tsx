c6920ca94edf5bffdc371e5f1a25c592bd8e3789
Sivaprasthv <sivaprasathv@sense7ai.com>
2023-08-16 19:10:07 +0530

    minor css fix

--- src/modules/bulkimportmodule/CandidateDatabaseTab.tsx
+++ src/modules/bulkimportmodule/CandidateDatabaseTab.tsx
@@ -574,7 +574,7 @@ const CandidateDatabaseTab = ({
         <Loader   size={'medium'} />
       </Flex>
       ) : (
-        <Flex width={window.innerWidth - 250} flex={1} style={{overflowX:'hidden'}}>
+        <Flex flex={1} style={{overflowX:'hidden'}}>
         <Tabel
           empty={
             isSearch === 1 ? 'No candidates imported yet' : 'No candidate found'
