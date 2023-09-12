21a13260ce69d9c5a2584fc69646957877f76bd4
Pugazh <pugazhe9202@gmail.com>
2023-09-12 11:49:54 +0530

    reworked date of 12-09-23

--- src/modules/meetingscheduler/events/CopyClipBoard.tsx
+++ src/modules/meetingscheduler/events/CopyClipBoard.tsx
@@ -7,9 +7,6 @@ import Button from '../../../uikit/Button/Button';
 import InputCheckBox from '../../../uikit/InputCheckbox/InputCheckBox';
 import styles from './daytimesplit.module.css';
 
-
-
-
 type CopyProps = {
     day: string;
     index: number;
@@ -72,7 +69,15 @@ const CopyClipBoard = ({
     setCopy,
     SetCopyId,
 }: CopyProps) => {
-    const [dataexist, setdataexist] = useState([]);
+    const daysOfWeek = [
+        { name: 'sunday', checked: sundaycheck, disabled: name === 'sunday',flag : false },
+        { name: 'monday', checked: mondaycheck, disabled: name === 'monday',flag : false  },
+        { name: 'tuesday', checked: tuesdaycheck, disabled: name === 'tuesday',flag : false  },
+        { name: 'wednesday', checked: wednesdaycheck, disabled: name === 'wednesday',flag : false  },
+        { name: 'thursday', checked: thursdaycheck, disabled: name === 'thursday',flag : false  },
+        { name: 'friday', checked: fridaycheck, disabled: name === 'friday',flag : false  },
+        { name: 'saturday', checked: saturdaycheck, disabled: name === 'saturday',flag : false  },
+    ];
 
     const [check1, setcheck1] = useState(false);
     const [check2, setcheck2] = useState(false);
@@ -82,41 +87,32 @@ const CopyClipBoard = ({
     const [check6, setcheck6] = useState(false);
     const [check7, setcheck7] = useState(false);
     const [onApply, setonApply] = useState(index);
+    const [DaysOfWeek,setDaysOfWeek] = useState(daysOfWeek)
 
 
-    const daysOfWeek = [
-        { name: 'sunday', checked: sundaycheck, disabled: name === 'sunday' },
-        { name: 'monday', checked: mondaycheck, disabled: name === 'monday' },
-        { name: 'tuesday', checked: tuesdaycheck, disabled: name === 'tuesday' },
-        { name: 'wednesday', checked: wednesdaycheck, disabled: name === 'wednesday' },
-        { name: 'thursday', checked: thursdaycheck, disabled: name === 'thursday' },
-        { name: 'friday', checked: fridaycheck, disabled: name === 'friday' },
-        { name: 'saturday', checked: saturdaycheck, disabled: name === 'saturday' },
-    ];
+    
 
     const handleCheckboxChange = (assignedvalue) => {
-        if (assignedvalue === 'sunday') {
-            setcheck1(!check1);
-        }
-        if (assignedvalue === 'monday') {
-            setcheck2(!check2);
-        }
-        if (assignedvalue === 'tuesday') {
-            setcheck3(!check3);
-        }
-        if (assignedvalue === 'wednesday') {
-            setcheck4(!check4);
-        }
-        if (assignedvalue === 'thursday') {
-            setcheck5(!check5);
-        }
-        if (assignedvalue === 'friday') {
-            setcheck6(!check6);
-        }
-        if (assignedvalue === 'saturday') {
-            setcheck7(!check7);
-        }
+        alert("assignedvalue"+assignedvalue)
+        const updatedDaysOfWeek = daysOfWeek.map((info,i)=> {
+            if (assignedvalue === info.name) {
+                console.log("info::::::",info)
+                return { ...info, flag: !info.flag };
+              }
+              return info;
+            })
+        setDaysOfWeek(updatedDaysOfWeek);
     };
+
+
+
+//   const handleCheckboxChange = (days) => {
+//     setCheckboxes((prevCheckboxes) => ({
+//       ...prevCheckboxes,
+//       [days]: !prevCheckboxes[days],
+//     }));
+//   };
+
 
     function closeButton() {
         setCopy(false);
@@ -225,7 +221,7 @@ const CopyClipBoard = ({
             setDay7(dayof);
         }
     };
-
+    console.log("check1",DaysOfWeek)
     return (
         <>
             <Flex title={'Copy timings'}>
@@ -252,7 +248,7 @@ const CopyClipBoard = ({
                                 }}
                             >
 
-                                {daysOfWeek.map((dayInfo, i) => (
+                                {DaysOfWeek.map((dayInfo, i) => (
                                     <Flex row center className={styles.copyalign} key={i}>
                                         {dayInfo.name === name ? (
                                             <InputCheckBox
@@ -262,7 +258,7 @@ const CopyClipBoard = ({
                                         ) : dayInfo.checked === true ? (
                                             <InputCheckBox
                                                 onChange={() => handleCheckboxChange(dayInfo.name)}
-                                                checked={dayInfo.disabled ? true : false}
+                                                checked={dayInfo.flag ? true : false}
                                             />
                                         ) : (
                                             <InputCheckBox
@@ -274,140 +270,6 @@ const CopyClipBoard = ({
                                     </Flex>
 
                                 ))}
-
-                                {/* <Flex row center className={styles.copyalign}>
-                    {name === 'sunday' ? (
-                      <InputCheckBox
-                        checked={name === 'sunday'}
-                        disabled={name === 'sunday'}
-                      />
-                    ) : sundaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('sunday')}
-                        checked={check1 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('sunday')}
-                        disabled={true}
-                      />
-                    )}
-                    <Text className={styles.space}>Sunday</Text>
-                  </Flex>
-                  <Flex row center className={styles.copyalign}>
-                    {name === 'monday' ? (
-                      <InputCheckBox
-                        checked={name === 'monday'}
-                        disabled={name === 'monday'}
-                      />
-                    ) : mondaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('monday')}
-                        checked={check2 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('monday')}
-                        disabled
-                      />
-                    )}
-                    <Text className={styles.space}>Monday</Text>
-                  </Flex>
-                  <Flex row center className={styles.copyalign}>
-                    {name === 'tuesday' ? (
-                      <InputCheckBox
-                        checked={name === 'tuesday'}
-                        disabled={name === 'tuesday'}
-                      />
-                    ) : tuesdaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('tuesday')}
-                        checked={check3 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('tuesday')}
-                        disabled={true}
-                      />
-                    )}
-                    <Text className={styles.space}>Tuesday</Text>
-                  </Flex>
-                  <Flex row center className={styles.copyalign}>
-                    {name === 'wednesday' ? (
-                      <InputCheckBox
-                        checked={name === 'wednesday'}
-                        disabled={name === 'wednesday'}
-                      />
-                    ) : wednesdaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('wednesday')}
-                        checked={check4 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('wednesday')}
-                        disabled={true}
-                      />
-                    )}
-                    <Text className={styles.space}>Wednesday</Text>
-                  </Flex>
-                  <Flex row center className={styles.copyalign}>
-                    {name === 'thursday' ? (
-                      <InputCheckBox
-                        checked={name === 'thursday'}
-                        disabled={name === 'thursday'}
-                      />
-                    ) : thursdaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('thursday')}
-                        checked={check5 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('thursday')}
-                        disabled={true}
-                      />
-                    )}
-                    <Text className={styles.space}>Thursday</Text>
-                  </Flex>
-                  <Flex row center className={styles.copyalign}>
-                    {name === 'friday' ? (
-                      <InputCheckBox
-                        checked={name === 'friday'}
-                        disabled={name === 'friday'}
-                      />
-                    ) : fridaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('friday')}
-                        checked={check6 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('friday')}
-                        disabled={true}
-                      />
-                    )}
-                    <Text className={styles.space}>Friday</Text>
-                  </Flex>
-                  <Flex row center className={styles.copyalign}>
-                    {name === 'saturday' ? (
-                      <InputCheckBox
-                        checked={name === 'saturday'}
-                        disabled={name === 'saturday'}
-                      />
-                    ) : saturdaycheck === true ? (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('saturday')}
-                        checked={check7 ? true : false}
-                      />
-                    ) : (
-                      <InputCheckBox
-                        onChange={() => handleCheckboxChange('saturday')}
-                        disabled={true}
-                      />
-                    )}
-                    <Text className={styles.space}>Saturday</Text>
-                  </Flex> */}
                                 <Flex className={styles.applybtn}>
                                     <Dropdown.Item className={styles.applybackground}>
                                         <Button
