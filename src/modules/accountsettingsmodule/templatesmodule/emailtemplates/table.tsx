57f567e251405f919dc34c2fe347e438b72cfe7f
PrasanthKumarV02 <prasanthkumarv@sense7ai.com>
2023-11-02 12:22:14 +0530

    Pop Up Fixes - 02112023

--- src/modules/accountsettingsmodule/templatesmodule/emailtemplates/table.tsx
+++ src/modules/accountsettingsmodule/templatesmodule/emailtemplates/table.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import { SvgEdit } from '../../../../icons';
+import SvgDelete from '../../../../icons/SvgDelete';
 import { Button, Text, Flex } from '../../../../uikit';
 import styles from '../emailtemplates/table.module.css'
+import TemplateDescriptionmodal from './templatedescriptionModal';
+
 type props = {
   handleBack: () => void;
 };
-const Table = () => {
 
+
+
+const Table = () => {
+  
+  const [isTempDescModal, setTempDescModal]=useState(false)
+  
+  const handleTempDescModal = () => {
+    setTempDescModal(!isTempDescModal)
+  }
 
   return (
 
@@ -46,9 +59,41 @@ const Table = () => {
         </th>
       </tr>
     </thead>
-    <tbody style={{ paddingTop: 20 }} className={styles.tablebody}></tbody>
+    <tbody style={{ paddingTop: 20 }} className={styles.tablebody}>
+      <tr style={{ height: 50 }}>
+        <td className={styles.padchang}>
+          <Flex onClick={handleTempDescModal}>
+            <Text bold color='theme'>
+              Template Description
+            </Text>
+          </Flex>
+        </td>
+        <td className={styles.padchang}>
+          Subject
+        </td>
+        <td className={styles.padchang}>
+          Created by
+        </td>
+        <td className={styles.padchang}>
+          Created On
+        </td>
+        <td className={styles.padchang}>
+        <Flex row className={styles.actionBtnContainer}>
+            <SvgEdit width={12} height={12} fill={'#581845'} />
+            <SvgDelete width={16} height={16} fill={'#581845'} />
+        </Flex>
+        </td>
+      </tr>
+    </tbody>
     {/* </Flex> */}
   </table>
+  <Flex>
+  {isTempDescModal && (
+          <>
+          <TemplateDescriptionmodal open={true} handleTempDescModal={handleTempDescModal}/>
+        </>
+        )}
+  </Flex>
 </Flex>
 
   )}
